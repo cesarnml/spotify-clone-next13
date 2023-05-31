9859684ee9dd9162a353a155ffa784fd53e6a95f
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-31 16:42:11 -0700

    Cesar/sc-249_implement-subscribe-modal-and-account-page (#6)
    * feat: 🛂 enable authentication via supabase
    
    * feat: ✨ complete auth user flow; install and implement toast package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { Sidebar } from '@/components/Sidebar'
-import './globals.css'
 import { Figtree } from 'next/font/google'
 import { SupabaseProvider } from '@/providers/SupabaseProvider'
 import { UserContextProvider } from '@/hooks/useUser'
+import { ModalProvider } from '@/providers/ModalProvider'
+import './globals.css'
+import { ToasterProvider } from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -15,8 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserContextProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserContextProvider>
         </SupabaseProvider>
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,11 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
 import { HiHome } from 'react-icons/hi'
 import { BiSearch } from 'react-icons/bi'
 import { Button } from './Button'
+import { useAuthModal } from '@/hooks/useAuthModal'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useUser } from '@/hooks/useUser'
+import { FaUserAlt } from 'react-icons/fa'
+import { toast } from 'react-hot-toast'
 
 type Props = {
   children: ReactNode
@@ -15,9 +20,23 @@ type Props = {
 
 export const Header = ({ children, className }: Props) => {
   const router = useRouter()
-  const handleLogout = () => {
-    // Handle logout in the future
+  const { onOpen } = useAuthModal()
+
+  const supabaseClient = useSupabaseClient()
+  const { user } = useUser()
+  console.log('user:', user)
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut()
+    // Reset any playing songs
+    router.refresh()
+    if (error) {
+      toast.error(error.message)
+    } else {
+      toast.success('Logged out!')
+    }
   }
+
   return (
     <div className={twMerge('h-fit bg-gradient-to-b from-emerald-800 p-6', className)}>
       <div className='w-full mb-4 flex items-center justify-between'>
@@ -38,14 +57,29 @@ export const Header = ({ children, className }: Props) => {
           </button>
         </div>
         <div className='flex justify-between items-center gap-x-4'>
-          <>
-            <div>
-              <Button className='bg-transparent text-neutral-300 font-medium'>Sign up</Button>
+          {user ? (
+            <div className='flex gap-x-4 items-center'>
+              <Button className='bg-white px-6 py-2' onClick={handleLogout}>
+                Logout
+              </Button>
+              <Button className='bg-white' onClick={() => router.push('/account')}>
+                <FaUserAlt />
+              </Button>
             </div>
-            <div>
-              <Button className='bg-white px-6 py-2'>Login</Button>
-            </div>
-          </>
+          ) : (
+            <>
+              <div>
+                <Button className='bg-transparent text-neutral-300 font-medium' onClick={onOpen}>
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button className='bg-white px-6 py-2' onClick={onOpen}>
+                  Login
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
