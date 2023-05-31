d75a72962d0537b5b21dca79cffad8090c11ad8d
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-31 05:56:49 -0700

    feat(ui): 💄 implement app layout

---
+++ components/Box.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+  children: ReactNode
+  className?: string
+}
+
+export const Box = ({ children, className }: Props) => {
+  return (
+    <div className={twMerge('bg-neutral-900 rounded-lg h-fit w-full', className)}>{children}</div>
+  )
+}
